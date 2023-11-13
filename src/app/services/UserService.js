f5c4f0fbdefcbd1a39359cf30260c734e38caf6f
lam nhat <zznhatphilong@gmail.com>
2023-11-13 18:52:12 +0700

    first commit

---
+++ src/app/services/UserService.js
@@ -0,0 +1,28 @@
+import Video from '../models/Video.js';
+import { multipleMongooesToOject } from '../../util/mongoose.js';
+
+class UserService {
+    index(req, res) {
+        Video.find({})
+            .then((videos) => {
+                res.render('user', {
+                    videos: multipleMongooesToOject(videos),
+                });
+            })
+            .catch((err) => {
+                res.status(400).json({ err: 'ERROR!' });
+            });
+    }
+
+    createVideo = async (payload) => {
+        try {
+            console.log('user service', payload);
+            const newVideo = await Video.insertMany(payload);
+            return newVideo;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+}
+
+export default new UserService();
