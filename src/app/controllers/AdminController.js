61ef5cadbd638aad240b2793ded387f85de4c6c1
Huyen Ho <hothihuyen06031195@gmail.com>
2023-11-14 10:10:53 +0700

    update adminController

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,12 +1,19 @@
-import AdminService from '../services/AdminService.js';
+import adminService from '../services/AdminService.js';
 
 class AdminController {
-    show(req, res) {
-        res.render('admin');
+    // [GET] /homepage
+    index(req, res) {
+        res.render('admin-user', { layout: 'admin.hbs', title: 'ADMIN-USER' });
     }
-
-    create(req, res) {
-        AdminService.createVideo(req, res);
+    // [GET] /homepage/:slug
+    showFood(req, res) {
+        res.render('admin-food', { layout: 'admin.hbs', title: 'ADMIN-FOOD' });
+    }
+    showVideo(req, res) {
+        adminService.showVideo(req, res);
+    }
+    showBlog(req, res) {
+        res.render('admin-blog', { layout: 'admin.hbs', title: 'ADMIN-BLOG' });
     }
 }
 
