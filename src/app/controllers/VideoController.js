250f905846048b39b73b728a749e98b0707b2bb1
quyenduc26 <quyenduc26@gmail.com>
2023-11-17 16:02:21 +0700

    update redirect

--- src/app/controllers/VideoController.js
+++ src/app/controllers/VideoController.js
@@ -1,21 +1,12 @@
 import VideoService from '../services/VideoService.js';
 
 class VideoController {
-<<<<<<< HEAD
-    show(req, res) {
-        VideoService.index(req, res);
-    }
-    index(req, res){
-        res.render('video');
-    }
-=======
 	show(req, res) {
 		VideoService.index(req, res);
 	}
 	index(req, res){
         res.render('/');
 	}
->>>>>>> 6eed831cefda4edafc6d83cd3f9273bbd8cce5ca
 }
 
 export default new VideoController();
