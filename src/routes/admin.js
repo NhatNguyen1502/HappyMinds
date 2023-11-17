ffc544ee0a97fd109d2f3056a9bcd3814a691247
Huyen Ho <hothihuyen06031195@gmail.com>
2023-11-18 03:02:58 +0700

    update feat save modify data

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import adminController from '../app/controllers/adminController.js';
+import adminController from '../app/controllers/AdminController.js';
 
 const router = express.Router();
 
 router.get('/', adminController.index);
 router.get('/admin-user', adminController.index);
 router.get('/admin-food', adminController.showFood);
-router.get('/admin-video', adminController.showVideo);
+router.get('/admin-video', adminController.showVideos);
 router.get('/admin-blog', adminController.showBlog);
 router.post('/video', adminController.createVideo);
 router.post('/user', adminController.createUser);
@@ -14,5 +14,7 @@ router.post('/food', adminController.createFood);
 router.post('/blog', adminController.createBlog);
 
 router.get('/create', (req, res) => res.render('admin'));
+
+router.post('/update-video/:id', adminController.updateVideo);
 
 export default router;
