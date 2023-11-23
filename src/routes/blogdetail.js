052fb0c7d95452c1aa100c019aaa6e7a2957fced
Nguyễn Lâm Nhật <116559833+NhatNguyen1502@users.noreply.github.com>
2023-11-23 22:23:05 +0700

    Feat/login logout gg (#29)
    * handle gg
    
    * done login
    
    * fix UI video
    
    ---------
    
    Co-authored-by: duytuanndt <tuannd@foxcode.org>

--- src/routes/blogdetail.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import BlogDetailCotroller from '../app/controllers/BlogDetailController.js';
-const router = express.Router();
-
-// router.get('/', blogDetailController);
-router.get('/:slug', BlogDetailCotroller.show);
-
-export default router;
--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -1,9 +1,41 @@
 import express from 'express';
 import homepageController from '../app/controllers/HomepageController.js';
+import passport from 'passport';
 
 const router = express.Router();
 
 router.get('/', homepageController.index);
+
 router.post('/checkBMI', homepageController.checkBMI);
+
+router.get('/login', function (req, res, next) {
+    if (!req.user) {
+        res.redirect('/');
+    } else {
+        res.redirect('/user');
+    }
+});
+
+router.get('/login/federated/google', passport.authenticate('google'));
+
+router.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/',
+    }),
+    function (req, res) {
+        req.session.user = req.user;
+        res.redirect('/user');
+    },
+);
+
+router.get('/logout', function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
 
 export default router;
