ca3c80827f14db161bb1f2f5f8ff1f5ebd73291a
Nguyễn Lâm Nhật <116559833+NhatNguyen1502@users.noreply.github.com>
2023-11-25 12:58:50 +0700

    Fix/color UI (#36)
    * done fix ui user

--- src/app/controllers/HomepageController.js
+++ src/app/controllers/HomepageController.js
@@ -1,15 +1,14 @@
 import homepageService from '../services/HomepageService.js';
 
 class HomepageController {
-    // [GET] /homepage
     index(req, res) {
         homepageService.index(req, res);
     }
 
-    showVideos(req, res){
+    showVideos(req, res) {
         homepageService.showVideos(req, res);
     }
-    showAllVideos(req, res){
+    showAllVideos(req, res) {
         homepageService.showAllVideos(req, res);
     }
 }
