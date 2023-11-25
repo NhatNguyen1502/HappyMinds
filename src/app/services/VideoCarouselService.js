c845a7838d46735cd1ffaec82915ce51f424ec17
hothihuyen <137026851+Hannah063@users.noreply.github.com>
2023-11-25 14:08:05 +0700

    Feat/redirect video carousel (#37)
    * appcss
    
    * fix link more info blog
    
    * add button start, redirect video to carousel page
    
    ---------
    
    Co-authored-by: Huyen Ho <hothihuyen06031195@gmail.com>

--- src/app/services/VideoCarouselService.js
+++ src/app/services/VideoCarouselService.js
@@ -3,15 +3,10 @@ import { multipleMongooesToOject } from '../../util/mongoose.js';
 
 class VideoCarouselService {
     index(req, res) {
-        Video.find({})
-            .then((videos) => {
-                res.render('videoCarousel', {
-                    videos: multipleMongooesToOject(videos).slice(0, 10),
-                });
-            })
-            .catch((err) => {
-                res.status(400).json({ err: 'ERROR!' });
-            });
+        const videoArrayJSON = req.body.array;
+        console.log(videoArrayJSON);
+        const videos = JSON.parse(videoArrayJSON);
+        res.render('videoCarousel', {videos});
     }
 }
 export default new VideoCarouselService();
