62a823e672b5a79d20d1dc795a806476d451066c
Nguyễn Lâm Nhật <116559833+NhatNguyen1502@users.noreply.github.com>
2023-11-26 08:18:17 +0700

    Feat/add food to menu (#44)
    * appcss
    
    * no change
    
    * fix link more info blog
    
    * add foodId to choseFoode
    
    * remove food and update
    
    * alert in food
    
    * add total
    
    ---------
    
    Co-authored-by: Huyen Ho <hothihuyen06031195@gmail.com>

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', userController.index);
 router.post('/', userController.updateUser);
+router.post('/:idFood', userController.removeFood);
 
 export default router;
