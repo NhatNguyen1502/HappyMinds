5a6b938d9c3be677dea012091b7704f150aefdbf
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2023-12-28 22:40:31 +0700

    blog pagination not completed yet

--- src/app/services/BlogService.js
+++ src/app/services/BlogService.js
@@ -20,6 +20,33 @@ class BlogService {
             });
     }
 
+// code test phân trang
+    // index(req, res) {
+    //     let isLogin = false;
+    //     if (req.isAuthenticated()) {
+    //         isLogin = true;
+    //     }
+
+    //     const currentPage = parseInt(req.query.page) || 1;
+    //     const itemsPerPage = 3;
+
+    //     blog.find({})
+    //         .skip((currentPage - 1) * itemsPerPage)
+    //         .limit(itemsPerPage)
+    //         .then((blogs) => {
+    //             blogs = multipleMongooesToOject(blogs);
+                
+    //             // Tính số lượng trang để hiển thị nút phân trang
+    //             blog.countDocuments().then((count) => {
+    //                 const totalPages = Math.ceil(count / itemsPerPage);
+    //                 res.render('blog', { blogs, isLogin, currentPage, totalPages });
+    //             });
+    //         })
+    //         .catch((err) => {
+    //             res.status(400).json({ err: 'ERROR!' });
+    //         });
+    // }
+
     showDetail(req, res) {
         let isLogin = false;
         if (req.isAuthenticated()) {
