634a0be6b46b8a241d1262b0d099dc2a8eec69c5
lam nhat <zznhatphilong@gmail.com>
2024-01-04 16:23:26 +0700

    not done

--- src/app/services/BlogService.js
+++ src/app/services/BlogService.js
@@ -1,4 +1,4 @@
-import blog from '../models/Blog.js';
+import Blog from '../models/Blog.js';
 import Comment from '../models/Comment.js';
 import {
     multipleMongooesToOject,
@@ -15,24 +15,28 @@ class BlogService {
         let currentPage = parseInt(req.query.page) || 1;
         const itemsPerPage = 3;
 
-        blog.countDocuments().then((count) => {
+        Blog.countDocuments().then((count) => {
             const totalPages = Math.ceil(count / itemsPerPage);
 
             if (currentPage <= 0) {
                 currentPage = 1;
-            }
-            else if (currentPage > totalPages) {
+            } else if (currentPage > totalPages) {
                 currentPage = totalPages;
             }
 
-            blog.find({})
+            Blog.find({})
                 .skip((currentPage - 1) * itemsPerPage)
                 .limit(itemsPerPage)
-                
+
                 .then((blogs) => {
                     blogs = multipleMongooesToOject(blogs);
 
-                    res.render('blog', { blogs, isLogin, currentPage, totalPages });
+                    res.render('blog', {
+                        blogs,
+                        isLogin,
+                        currentPage,
+                        totalPages,
+                    });
                 })
                 .catch((err) => {
                     res.status(400).json({ err: 'ERROR!' });
@@ -40,26 +44,27 @@ class BlogService {
         });
     }
 
-    showDetail(req, res) {
+    async showDetail(req, res) {
         let isLogin = false;
         if (req.isAuthenticated()) {
             isLogin = true;
         }
-        blog.findOne({ slug: req.params.slug })
+
+        Blog = await blog.findOne({ slug: req.params.slug });
+        console.log('content = ', blog.content);
+        Comments = Blog.findOne({ slug: req.params.slug })
             .then((blog) => {
-                console.log("id = ",blog._id);
-                Comment.findOne({ idBlog: blog._id})
-                    .then(comment => {
-                        console.log("comment = ",comment);
-                        res.render('blogDetail', {
-                            blog: mongooesToOject(blog),
-                            isLogin,
-                            comment: mongooesToOject(comment),
-                        });
-                    })
+                Comment.findOne({ idBlog: blog._id }).then((comment) => {
+                    console.log('comment = ', comment);
+                    res.render('blogDetail', {
+                        blog: mongooesToOject(blog),
+                        isLogin,
+                        comment: mongooesToOject(comment),
+                    });
+                });
             })
             .catch((err) => {
-                res.status(400).json({ err: 'ERROR!' }); 
+                res.status(400).json({ err: 'ERROR!' });
             });
     }
 }
