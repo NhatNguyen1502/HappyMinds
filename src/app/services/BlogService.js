9bf31dfb822e1eafa9c0d5ac2cd496834b64f62d
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2024-01-05 10:54:42 +0700

    fix reload blog page for the first time

--- src/app/services/BlogService.js
+++ src/app/services/BlogService.js
@@ -6,34 +6,39 @@ import {
 
 class BlogService {
     index(req, res) {
-        let isLogin = false;
-        if (req.isAuthenticated()) {
-            isLogin = true;
-        }
-
-        let currentPage = parseInt(req.query.page) || 1;
         const itemsPerPage = 3;
 
         blog.countDocuments().then((count) => {
             const totalPages = Math.ceil(count / itemsPerPage);
 
-            if (currentPage <= 0) {
-                currentPage = 1;
-            }
-            else if (currentPage > totalPages) {
-                currentPage = totalPages;
-            }
+            blog.find({})
+                .limit(itemsPerPage)
+                .then((blogs) => {
+                    blogs = multipleMongooesToOject(blogs);
+                    res.render('blog', { blogs, totalPages });
+                })
+                .catch((err) => {
+                    res.status(500).json({ err: 'ERROR!' });
+                });
+        });
+    }
+
+    showPanigation(req, res) {
+        let currentPage = parseInt(req.query.page);
+        const itemsPerPage = 3;
+
+        blog.countDocuments().then((count) => {
+            const totalPages = Math.ceil(count / itemsPerPage);
 
             blog.find({})
                 .skip((currentPage - 1) * itemsPerPage)
                 .limit(itemsPerPage)
+                .lean()
                 .then((blogs) => {
-                    blogs = multipleMongooesToOject(blogs);
-
-                    res.render('blog', { blogs, isLogin, currentPage, totalPages });
+                    res.json({ blogs, totalPages });
                 })
                 .catch((err) => {
-                    res.status(400).json({ err: 'ERROR!' });
+                    res.status(500).json({ err: 'ERROR!' });
                 });
         });
     }
