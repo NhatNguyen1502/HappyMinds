efd639208042213ce7266e94fff08cf632feaf1d
Nguyễn Lâm Nhật <116559833+NhatNguyen1502@users.noreply.github.com>
2024-01-10 05:20:29 +0700

    Feat/like comment (#84)
    * done like comment
    
    ---------
    
    Co-authored-by: quyenduc26 <quyenduc26@gmail.com>

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -5,11 +5,17 @@ import fileUploader from '../middlerwares/cloudinary.js';
 const router = express.Router();
 
 router.get('/getBlogComments', CommentController.getBlogComments);
+
 router.get('/getReplyComments', CommentController.getReplyComments);
+
 router.post(
     '/',
     fileUploader.single('imgUrl'),
     CommentController.createComment,
 );
+
+router.patch('/addLike', CommentController.addLike);
+
+router.patch('/removeLike', CommentController.removeLike);
 
 export default router;
