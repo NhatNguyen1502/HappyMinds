d5ac255f5118799554e3068bf298b8e66e3a801b
phatdeptraiii <vitphat350@gmail.com>
2024-01-17 09:03:09 +0700

    merge develop to feat/newPost

--- src/app/services/AdminService.js
+++ src/app/services/AdminService.js
@@ -1,59 +1,67 @@
-import Video from '../models/Video.js';
-import User from '../models/User.js';
-import Food from '../models/Food.js';
-import Blog from '../models/Blog.js';
-import Comment from '../models/Comment.js';
-import { multipleMongooesToOject } from '../../util/mongoose.js';
-import { response } from 'express';
+import Video from "../models/Video.js";
+import User from "../models/User.js";
+import Food from "../models/Food.js";
+import Blog from "../models/Blog.js";
+import { generateTitle } from "../../util/generateSlug.js";
+import { removeVietnameseTones } from "../../util/generateSlug.js";
+import { Slug } from "../../util/generateSlug.js";
+
+import { multipleMongooesToOject } from "../../util/mongoose.js";
 
 class AdminService {
-    createVideo = async (req, res) => {
-        const formData = req.body;
-        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLA3BDVGf8B0taC4h-qjJm4wYxbnsw`;
-        const saveVideo = await Video.create(formData);
-        saveVideo
-            .save()
-            .then(() => res.redirect('/admin/admin-video'))
-            .catch((err) => console.log(err));
-    };
-    createUser = async (req, res) => {
-        const formData = req.body;
-        const saveUser = await User.create(formData);
-        saveUser
-            .save()
-            .then(() => res.redirect('/admin/admin-user'))
-            .catch((err) => console.log(err));
-    };
-    createFood = async (req, res) => {
-        const formData = req.body;
-        const saveFood = await Food.create(formData);
-        saveFood
-            .save()
-            .then(() => res.redirect('/admin/admin-food'))
-            .catch((err) => console.log(err));
-    };
-    createBlog = async (req, res) => {
-        const formData = req.body;
-        const saveBlog = await Blog.create(formData);
-        saveBlog
-            .save()
-            .then(() => res.redirect('/admin/admin-blog'))
-            .catch((err) => console.log(err));
-    };
-    showVideos = async (req, res) => {
-        const page = parseInt(req.query.page) || 1;
-        const perPage = 10;
-        Video.find({})
-            .skip((page - 1) * perPage)
-            .limit(perPage)
-            .then((videos) => {
-                res.render('admin-video', {
-                    videos: multipleMongooesToOject(videos),
-                    layout: 'admin.hbs',
-                    title: 'ADMIN-VIDEO',
-                });
-            });
-    };
+	createVideo = async (req, res) => {
+		const formData = req.body;
+		formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLA3BDVGf8B0taC4h-qjJm4wYxbnsw`;
+		const saveVideo = await Video.create(formData);
+		saveVideo
+			.save()
+			.then(() => res.redirect("/admin/admin-video"))
+			.catch((err) => console.log(err));
+	};
+	createUser = async (req, res) => {
+		const formData = req.body;
+		const saveUser = await User.create(formData);
+		saveUser
+			.save()
+			.then(() => res.redirect("/admin/admin-user"))
+			.catch((err) => console.log(err));
+	};
+	createFood = async (req, res) => {
+		const formData = req.body;
+		const saveFood = await Food.create(formData);
+		saveFood
+			.save()
+			.then(() => res.redirect("/admin/admin-food"))
+			.catch((err) => console.log(err));
+	};
+	createBlog = async (req, res) => {
+		const formData = req.body;
+		formData.title = generateTitle(req.body.title);
+		let oldSlug = removeVietnameseTones(req.body.title);
+		let newSlug = Slug.generateSlug(oldSlug);
+		console.log(newSlug);
+		formData.slug = newSlug;
+		const saveBlog = await Blog.create(formData);
+		console.log(formData)
+		saveBlog
+			.save()
+			.then(() => res.redirect("/admin/admin-blog"))
+			.catch((err) => console.log(err));
+	};
+	showVideos = async (req, res) => {
+		const page = parseInt(req.query.page) || 1;
+		const perPage = 10;
+		Video.find({})
+			.skip((page - 1) * perPage)
+			.limit(perPage)
+			.then((videos) => {
+				res.render("admin-video", {
+					videos: multipleMongooesToOject(videos),
+					layout: "admin.hbs",
+					title: "ADMIN-VIDEO",
+				});
+			});
+	};
 
     updateVideo = async (req, res) => {
         try {
